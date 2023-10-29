9f20ab2cfb17e60fdbe9e0d4359bd74c16644b66
Kirill Berezin <berezk7@gmail.com>
2023-10-29 12:45:28 +0400

    Добавьте файлы проекта.

---
+++ MultiThread/MultiThread.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void task() {
+    int n = 100000;
+    while (n != 0) {
+        n -= 1;
+    }
+}
+
+int main() {
+    thread thread1(task);
+    thread thread2(task);
+
+    thread1.join();
+    thread2.join();
+
+    return 0;
+}
